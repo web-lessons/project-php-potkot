d6e894d66e51c0e29b9cba60bf1575d2073c190c
Konstantin Potapov <k.m.potapov@mail.ru>
2020-04-11 21:07:07 +0700

    Урок 11,04,2020 конец

--- admin/products/edit.php
+++ admin/products/edit.php
@@ -9,20 +9,35 @@ if (!isLogin()) {
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
 if (!empty($_POST)):
+
     if (!empty($_FILES["img"]['name'])):
         $ext = pathinfo($_FILES['img']['name'], PATHINFO_EXTENSION);
         $fileName = uniqid().".".$ext;
-        $uploadFile = __DIR__."/img/".$fileName;
+        $uploadFile = __DIR__."/../../img/".$fileName;
         move_uploaded_file($_FILES['img']['tmp_name'], $uploadFile);
     endif;
+
     $name = $_POST['name'];
     $slug = $_POST['slug'];
     $img = empty($fileName) ? null : '/img/'.$fileName;
     $price = empty($_POST['price']) ? 0 : $_POST['price'];
     $description = empty($_POST['description']) ? null : $_POST['description'];
 
-    $sql = "INSERT INTO products (name, slug, img, description, price) 
+    if (!empty($_POST['id'])):
+
+        $addImg = "img='{$img}', ";
+
+        if (empty($img)) {
+            $addImg = "";
+        }
+
+        $sql = "UPDATE products 
+                 SET name='{$name}', slug='{$slug}', ".$addImg."  description='{$description}', price={$price} 
+                 WHERE id=".$_POST['id'];
+    else:
+        $sql = "INSERT INTO products (name, slug, img, description, price) 
         VALUES ('{$name}', '{$slug}', '{$img}', '{$description}', {$price})";
+    endif;
 
     $result = mysqli_query($conn, $sql);
 endif;
@@ -56,27 +71,31 @@ if (!empty($_GET['id'])) {
         <div class="col-md-6">
             <h1>Редактирование товара</h1>
             <form class="mb-4" method="post" enctype="multipart/form-data">
-
-
+                <? if (!empty($product)): ?>
+                    <input type="hidden" name="id" value="<?= $product['id'] ?>">
+                <? endif; ?>
                 <div class="form-group">
                     <label for="name">Название <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="name" name="name" required>
+                    <input type="text" <?= empty($product) ? "" : "value='".$product['name']."'" ?> class="form-control" id="name" name="name" required>
                 </div>
                 <div class="form-group">
                     <label for="slug">Код <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="slug" name="slug" required>
+                    <input type="text" <?= empty($product) ? "" : "value='".$product['slug']."'" ?> class="form-control" id="slug" name="slug" required>
                 </div>
                 <div class="form-group">
                     <label for="price">Цена</label>
-                    <input type="number" class="form-control" id="price" name="price">
+                    <input type="number" <?= empty($product) ? "" : "value='".$product['price']."'" ?> class="form-control" id="price" name="price">
                 </div>
                 <div class="form-group">
+                    <? if (!empty($product['img'])): ?>
+                        <img class="img-thumbnail" width="150px;" src="<?= $product['img'] ?>"><br>
+                    <? endif; ?>
                     <label for="img">Картинка</label>
                     <input type="file" class="form-control-file" id="img" name="img">
                 </div>
                 <div class="form-group">
                     <label for="description">Описание</label>
-                    <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                    <textarea class="form-control" id="description" rows="3" name="description"><?= empty($product) ? "" : $product['description'] ?></textarea>
                 </div>
                 <button type="submit" class="btn btn-primary">Добавить</button>
             </form>
