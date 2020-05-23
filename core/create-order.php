11c6ec6f4c40935bca2c49a4b0975de166e1276b
Konstantin Potapov <k.m.potapov@mail.ru>
2020-05-23 20:56:30 +0700

    Создние заказа начало 2

--- core/create-order.php
+++ core/create-order.php
@@ -4,13 +4,14 @@ include_once __DIR__.'/../config/db.php';
 
 if (!empty($_POST)) {
 
-    $date = $_POST['date'];
+    $date = date('Y-m-d H:i:s', strtotime($_POST['date']));
     $comment = empty($_POST['comment']) ? "" : trim($_POST['comment']);
     $address = empty($_POST['address']) ? "" : trim($_POST['address']);
     $delivery = empty($_POST['delivery']) ? 0 : 1;
     $pay = $_POST['pay'];
     $userId = $_SESSION['id'];
 
-    $sql = "";
+    $sql = "INSERT INTO orders 
+            (user_id, date, comment, address, delivery, pay) ";
 
 }
